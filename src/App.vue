b8414d6cadacaf6e9a54f941ad6f6da730b80acf
Arvin Indrawan <48991518+ArvinVin@users.noreply.github.com>
2021-03-17 13:10:00 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/profile">Profile</router-link>
+  <div id="app" class="app">
+    <div class="app__container">
+      <header class="app__header">
+        <AppHeader />
+      </header>
+
+      <div class="app__pages">
+        <router-view class="app__content"/>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+const AppHeader = () => import(/* webpackChunkName: "c-app-header" */ '@/components/AppHeader.vue')
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  components: {
+    AppHeader
+  }
 }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  @import "~@/assets/css/_vars.scss";
+
+  .app {
+    &__container {
+    }
+
+    &__content {
+      max-width: 1200px;
+      background-color: $color-black;
+      padding: 25px 0;
+      margin: 0 auto;
+    }
+  }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-header">
+      <div class="app-header__container">
+        ini header
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-header {
+      &__container {
+          max-width: 1200px;
+          background-color: #0095da;
+          margin: 0 auto;
+      }
+  }
+</style>
